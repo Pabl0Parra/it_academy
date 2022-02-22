c383d48bd201777b60008f038d7ba70e4423d8a5
Pablo Parra <pparrita2001@gmail.com>
2022-02-22 16:51:31 +0100

    fixed formula problem

--- exercises/Mod_7_3/exercise_7_3_7/script_7_3_7.js
+++ exercises/Mod_7_3/exercise_7_3_7/script_7_3_7.js
@@ -5,31 +5,31 @@ const SHRIMPPERSERVING = 200 / 4;
 const KILOGRAM = 1000;
 
 function calculatePrice() {
-  let foodies = Number(document.getElementById("id_foodies").value);
-  let noodlesPrice = Number(document.getElementById("id_noodles").value);
-  let squidPrice = Number(document.getElementById("id_squid").value);
-  let shrimpPrice = Number(document.getElementById("id_shrimp").value);
+  const FOODIES = Number(document.getElementById("id_foodies").value);
+  const NOODLESPRICE = Number(document.getElementById("id_noodles").value);
+  const SQUIDPRICE = Number(document.getElementById("id_squid").value);
+  const SHRIMPRICE = Number(document.getElementById("id_shrimp").value);
 
-  const TOTALAMOUNTNOODLES = NOODLESPERSERVING * foodies;
-  const TOTALAMOUNTSQUIDS = SQUIDPERSERVING * foodies;
-  const TOTALAMOUNTSHRIMPS = SHRIMPPERSERVING * foodies;
+  const TOTALAMOUNTNOODLES = (NOODLESPERSERVING / KILOGRAM) * FOODIES;
+  const TOTALAMOUNTSQUIDS = (SQUIDPERSERVING / KILOGRAM) * FOODIES;
+  const TOTALAMOUNTSHRIMPS = (SHRIMPPERSERVING / KILOGRAM) * FOODIES;
 
   document.getElementById(
     "id_totalingredients"
   ).innerHTML = `To make this fideua, we are going to need ${TOTALAMOUNTNOODLES} kgs of noodles, also ${TOTALAMOUNTSQUIDS} kgs of squid and ${TOTALAMOUNTSHRIMPS} kgs of shrimps`;
 
-  const TOTALNOODLESPRICE = TOTALAMOUNTNOODLES * (noodlesPrice / KILOGRAM);
-  const TOTALSQUIDPRICE = TOTALAMOUNTSQUIDS * (squidPrice / KILOGRAM);
-  const TOTALSHRIMPSPRICE = TOTALAMOUNTSHRIMPS * (shrimpPrice / KILOGRAM);
+  const TOTALNOODLESPRICE = TOTALAMOUNTNOODLES * NOODLESPRICE;
+  const TOTALSQUIDPRICE = TOTALAMOUNTSQUIDS * SQUIDPRICE;
+  const TOTALSHRIMPSPRICE = TOTALAMOUNTSHRIMPS * SHRIMPRICE;
 
   const TOTALFIDEUAPRICE =
     TOTALNOODLESPRICE + TOTALSQUIDPRICE + TOTALSHRIMPSPRICE;
-  const PRICEPERSERVING = TOTALFIDEUAPRICE / foodies;
+  const PRICEPERSERVING = TOTALFIDEUAPRICE / FOODIES;
 
   document.getElementById(
     "id_priceperserving"
   ).innerHTML = `The price per serving is €${PRICEPERSERVING}`;
   document.getElementById(
     "id_totalfideuaprice"
-  ).innerHTML = `The cost of the fideua for ${foodies} is €${TOTALFIDEUAPRICE}`;
+  ).innerHTML = `The cost of the fideua for ${FOODIES} is €${TOTALFIDEUAPRICE}`;
 }
