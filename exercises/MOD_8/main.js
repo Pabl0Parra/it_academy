21463b362fc911bd33f2c99ccc9dfcb75ac28b33
Pablo Parra <pparrita2001@gmail.com>
2022-03-03 09:10:47 +0100

    improved 8.2, missing a loop for the hotellist

--- exercises/MOD_8/main.js
+++ exercises/MOD_8/main.js
@@ -1,36 +1,119 @@
-import { Hotel } from "./hotel.js";
+let hotelsList = [];
 
-const REGISTER_BUTTON = document.getElementById("id_register");
-REGISTER_BUTTON.addEventListener("click", () => {
-  createHotel();
-});
+class Hotel {
+  name;
+  numberOfRooms;
+  numberOfFloors;
+  totalArea;
 
-function createHotel() {
-  const name = document.getElementById("id_name").value;
-  const numberOfRooms = Number(document.getElementById("id_rooms").value);
-  const numberOfFloors = Number(document.getElementById("id_floors").value);
-  const totalArea = Number(document.getElementById("id_area").value);
+  constructor(name, numberOfRooms, numberOfFloors, totalArea) {
+    this.name = name;
+    this.numberOfRooms = numberOfRooms;
+    this.numberOfFloors = numberOfFloors;
+    this.totalArea = totalArea;
+  }
+  get name() {
+    return this._name;
+  }
+  get numbersOfRooms() {
+    return this._numberOfRooms;
+  }
+  get numberOfFloors() {
+    return this._numberOfFloors;
+  }
+  get totalArea() {
+    return this._totalArea;
+  }
+  set name(newName) {
+    return (this.name = newName);
+  }
+  set numberOfRooms(newRooms) {
+    return (this.numberOfRooms = newRooms);
+  }
+  set numberOfFloors(newFloors) {
+    return (this.numberOfFloors = newFloors);
+  }
+  set totalArea(newArea) {
+    return (this.totalArea = newArea);
+  }
+  calculateLaborCost() {
+    const NUMBER_OF_EMPLOYEES = this.numberOfRooms / 20;
+    const TOTAL_COST = NUMBER_OF_EMPLOYEES * 1500;
 
-  const registeredHotel = new Hotel({
-    name: name,
-    numberOfRooms: numberOfRooms,
-    numberOfFloors: numberOfFloors,
-    totalArea: totalArea,
-  });
+    return `The monthly labor cost for the room service department of the ${this.name} hotel is ${TOTAL_COST}`;
+  }
+}
 
-  let hotelsList = [];
+function registerHotel() {
+  const name = document.getElementById("name").value;
+  const numberOfRooms = Number(document.getElementById("rooms").value);
+  const numberOfFloors = Number(document.getElementById("floors").value);
+  const totalArea = Number(document.getElementById("area").value);
+
+  if (
+    name == "" ||
+    numberOfRooms == "" ||
+    numberOfFloors == "" ||
+    totalArea == ""
+  ) {
+    alert("Please fill all the fields!");
+  } else {
+    alert("The hotel has been submitted");
+  }
+
+  const registeredHotel = new Hotel(
+    name,
+    numberOfRooms,
+    numberOfFloors,
+    totalArea
+  );
+
   hotelsList.push(registeredHotel);
 
-  let displayHotels = `The registered hotels are: `;
+  displayHotels = `The registered hotels are: `;
 
-  for (let i = 0; i < hotelsList.length; i++) {
+  console.log(hotelsList.name);
+
+  for (i = 0; i < hotelsList.length; i++) {
     displayHotels += `${hotelsList[i].name}, `;
   }
-  console.log(hotelsList);
-  console.log(displayHotels);
-  //   document.getElementById(
-  //     "id_registered_hotels"
-  //   ).innerHTML = `${displayHotels}`;
+
+  document.getElementById("listHotels").innerHTML = `${displayHotels}`;
 
   return registeredHotel;
+}
+
+function deleteHotel() {
+  const nameOfCancelHotel = document.getElementById("byeHotel").value;
+
+  let validateHotel = false;
+  let namePosition = 0;
+
+  for (element of hotelsList) {
+    if (element.name === nameOfCancelHotel) {
+      validateHotel = true;
+      console.log("Validate true");
+      break;
+    }
+    console.log(namePosition++);
+  }
+
+  if (validateHotel) {
+    hotelsList.splice(namePosition, 1);
+  } else {
+    alert(`Hotel not found in hotel list`);
+  }
+
+  displayHotels = "Current Hotels: ";
+
+  for (i = 0; i < hotelsList.length; i++) {
+    displayHotels += `${hotelsList[i].name}, `;
+  }
+
+  document.getElementById("updatedHotelList").innerHTML = `${displayHotels}`;
+  alert(`The hotel has been deleted`);
+}
+
+function showHotel() {
+  const HOTEL_SHOWING = document.getElementById("displayed").value;
 }
